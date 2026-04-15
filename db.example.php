2000dfc1fe0a7e73bedc2340b3e5f5bde24f9f21
zahavi <benjamin.zahavi@hotmail.fr>
2026-04-15 12:53:29 +0200

    fix: anonymisation complète du fichier d'exemple

--- db.example.php
+++ db.example.php
@@ -3,9 +3,7 @@ $host = "localhost";
 $user = "TON_UTILISATEUR";
 $pass = "TON_MOT_DE_PASSE";
 $db   = "TA_BASE_DE_DONNEES";
-$user = "roni4736_zahavi_benjamin";
-$pass = "*******";
-$db   = "roni4736_zahavi_benjamin_ue4";
+
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
